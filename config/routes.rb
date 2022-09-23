6fabe7c46ad26693b39a1ca18427e3c544ea347c
”Alexander <omgout.200@gmail.com>
2022-09-23 19:45:02 +0700

    sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :posts
-  resources :users
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
 
   get "/register", to: "users#new"
+  get "/sign_in", to: "sessions#new"
 
 
 end
